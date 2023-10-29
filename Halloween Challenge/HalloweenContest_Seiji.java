cfd541b82d9d4696285ecb54dafebcb69e604b19
Cynicality-1 <145064181+Cynicality-1@users.noreply.github.com>
2023-10-29 17:48:51 -0400

    Create HalloweenContest_Seiji.java

---
+++ Halloween Challenge/HalloweenContest_Seiji.java
@@ -0,0 +1,69 @@
+public class HalloweenContest {
+	static String[] ghost = {
+"       .-.    ",
+"      | o |   ",
+"    |^_.' '._|^ ",
+"   |         |  ",
+"    ||       |  ",
+"     ||    |`   ",
+"   |__|  |      ",
+"   *.__.'       "};
+	static int space = 0; //Space between ghost and left side of window
+	
+	public static void main(String[] args) throws InterruptedException {
+		int counter = 15; //Integer counter to prevent infinite loop
+		
+		while (counter > 0)	{ //Main loop of system
+			
+			for (int i = 0; i < 3; i++) { //loop for ghost to go forward
+				clearScreen();
+				printGhost();
+				space++;
+				Thread.sleep(400);
+			}
+			flipGhost();
+			
+			for (int i = 0; i < 3; i++) { //loop for ghost to go backward
+				clearScreen();
+				printGhost();
+				space--;
+				Thread.sleep(400);
+			}
+			flipGhost();
+			
+			counter--;
+		}
+	}
+	
+	public static void printGhost() {
+		System.out.println("  HAPPY HALLOWEEN!\n");
+		
+		for (String i : ghost) { //"For i in each" loop to iterate over String array "ghost"
+			for (int j = space; j > 0; j--) {
+				System.out.print(" ");
+			}
+			System.out.println(i); //Prints out each 
+		}
+	}
+	
+	public static void clearScreen() {
+		for (int i = 0; i < 20; i++) {
+			System.out.println(""); //Prints blank 20 times to move to next "frame"
+		}
+	}
+	public static String reverseString(String input) { //Method to reverse current string
+		String output = "";
+		for (int i = input.length(); i > 0; i--) {
+			output += input.charAt(i-1);
+		}
+		return output;
+	}
+	public static void flipGhost() {
+		int index = 0;
+		for (String i : ghost) {
+			ghost[index] = reverseString(i);
+			index++;
+		}
+	}
+	
+}
