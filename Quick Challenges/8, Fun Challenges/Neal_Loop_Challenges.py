9eede6c993ffd9462a93ea8b130847bac27833dd
Pirate373 <120601835+Pirate373@users.noreply.github.com>
2023-10-30 11:02:39 -0400

    Neal_Loop_Challenges.py

--- Quick Challenges/8, Fun Challenges/Neal_Loop_Challenges.py
+++ Quick Challenges/8, Fun Challenges/Neal_Loop_Challenges.py
@@ -1,16 +1,17 @@
+#Factorial
 numf = int(input("Enter A Number: \n"))
 factorial = 1
 
 if numf < 0:
    print("Error 5518, Please Try Another Input")
 elif numf == 0:
-   print("1, Please Rate Us At Go.Rate/789h.com")
+   print("1")
 else:
    for i in range(1,numf + 1):
        factorial = factorial*i
-   print(factorial, ", Please Rate Us At Go.Rate/789h.com")
+   print(factorial)
 
-
+#Fibonacci
 nterms = int(input("Enter Number Of Terms: \n"))
 n1, n2 = 0, 1
 count = 0
